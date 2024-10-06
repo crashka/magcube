60f65a2422c7b9bafa2209407a65441674161ceb
crash <ltung783@gmail.com>
2024-10-06 15:14:36 -0700

    refactor in preparation for creating new models (that might actually work, haha)

--- magcube.py
+++ magcube.py
@@ -4,12 +4,142 @@
 """Solve the purple magnetic cube puzzle in my mom's living room using CP-SAT.
 """
 
+from typing import Self
 import sys
 from os import environ
 
-from ortools.sat.python import cp_model
+from ortools.sat.python.cp_model import IntVar, Domain, CpModel, CpSolver, OPTIMAL, FEASIBLE
 
 DEBUG = int(environ.get('MAGCUBE_DEBUG') or 0)
+
+CoordT = tuple[int, ...]
+BlockT = CoordT
+PieceT = tuple[BlockT, ...]
+
+# all valid coordinates for the puzzle
+COORDS = ((x, y, z) for x in range(3) for y in range(3) for z in range(3))
+
+class ModelBase(CpModel):
+    """Abstract base class for local models.
+    """
+    pieces:   list[PieceT]
+    at_coord: dict[CoordT, list[int]]  # value: list of piece IDs
+    model:    CpModel
+    solver:   CpSolver
+
+    def __init__(self, pieces: list[PieceT]):
+        """Constructor takes list of pieces as input.
+        """
+        super().__init__()
+        self.pieces = pieces
+        self.at_coord = {coord: [] for coord in COORDS}
+        for p_id, piece in enumerate(pieces):
+            for block_coord in piece:
+                self.at_coord[block_coord].append(p_id)
+
+        self.model = CpModel()
+        self.solver = None
+
+    def build(self) -> Self:
+        """Add variables and constraints for the model.  Abstract method--must be
+        implemented by the subclass.  Return ``self``, for method chaining.
+        """
+        raise NotImplementedError("Can't call abstract method")
+
+    def solve(self) -> bool:
+        """Return ``True`` if solution is found; ``False`` otherwise.
+        """
+        self.solver = CpSolver()
+        if DEBUG:
+            self.solver.parameters.log_search_progress = True
+            if DEBUG > 1:
+                self.solver.parameters.log_subsolver_statistics = True
+        status = self.solver.solve(self.model)
+        print(f"Status: {status} ({self.solver.status_name()})", file=sys.stderr)
+        if info := self.solver.solution_info():
+            print(f"Solution info: {info}", file=sys.stderr)
+        return status in (OPTIMAL, FEASIBLE)
+
+    def solution(self) -> list[int]:
+        """Return list of pieces for the solution.  Abstract method--must be implemented
+        by the subclass.
+        """
+        raise NotImplementedError("Can't call abstract method")
+
+    def print_stats(self) -> None:
+        """Print solver stats, for benchmarking and/or analysis.
+        """
+        if not self.solver:
+            raise RuntimeError("Must solve before stats are available")
+
+        print("\nSolver Stats", file=sys.stderr)
+        print(f"- Conflicts : {self.solver.num_conflicts}", file=sys.stderr)
+        print(f"- Branches  : {self.solver.num_branches}", file=sys.stderr)
+        print(f"- Wall time : {self.solver.wall_time:.2f} secs", file=sys.stderr)
+
+class Model0(ModelBase):
+    """Constraints based on domains and combinations (``add_allowed_assignments()``).
+    """
+    blocks: dict[tuple, IntVar]
+    piece_usage: list[IntVar]
+
+    def build(self) -> Self:
+        """Add variables and constraints for the model.  Return ``self``, for method
+        chaining.
+        """
+        # Constraint #1 - The value for each block is constrained to the IDs for the pieces
+        # that can occupy it
+        self.blocks = {}
+        for coord in COORDS:
+            domain = Domain.from_values(self.at_coord[coord])
+            self.blocks[coord] = self.model.new_int_var_from_domain(domain, f'block_{coord}')
+
+        # Constraint #2 - Number of pieces (distinct values) in the solution must equal 9
+        self.piece_usage = []
+        for p_id, piece in enumerate(self.pieces):
+            p_used = self.model.new_bool_var(f'used_{p_id}')
+            self.model.add(p_used == any(var == p_id for var in self.blocks.values()))
+            self.piece_usage.append(p_used)
+        self.model.add(sum(self.piece_usage) == 9)
+        return self
+
+    def solution(self) -> list[int]:
+        """Return list of pieces for the solution.
+        """
+        if not self.solver:
+            raise RuntimeError("Must solve before solution is available")
+
+        solution = [str(p_used) for p_used in self.piece_usage if self.solver.value(p_used)]
+        return solution
+
+class Model1(ModelBase):
+    """Constraints based on domains and combinations (``add_allowed_assignments()``).
+    """
+    def build(self) -> Self:
+        """Add variables and constraints for the model.  Return ``self``, for method
+        chaining.
+        """
+        return self
+
+    def solution(self) -> list[int]:
+        """Return list of pieces for the solution.
+        """
+        return None
+
+class Model2(ModelBase):
+    """Constraints based on propositional logic and reification (``add_bool_and()`` and
+    ``only_enforce_if()``).
+    """
+    def build(self) -> Self:
+        """Add variables and constraints for the model.  Return ``self``, for method
+        chaining.
+        """
+        return self
+
+    def solution(self) -> list[int]:
+        """Return list of pieces for the solution.
+        """
+        return None
 
 def build_pieces() -> list:
     """Generate full list of magnetically correct pieces
@@ -69,49 +199,13 @@ def fit_pieces(pieces: list) -> list | None:
     to ensure that the right number of pieces are selected, and the block values (piece
     IDs) are compatible with the location of the block.
     """
-    npieces = len(pieces)
-
-    coords = ((x, y, z) for x in range(3) for y in range(3) for z in range(3))
-    at_coord = {coord: [] for coord in coords}  # value: list of piece IDs
-    for p_id, piece in enumerate(pieces):
-        for block_coord in piece:
-            at_coord[block_coord].append(p_id)
-    domains = {coord: cp_model.Domain.from_values(at_coord[coord]) for coord in coords}
-
-    model = cp_model.CpModel()
-
-    # Constraint #1 - The value for each block is constrained to the IDs for the pieces
-    # that can occupy it
-    blocks = {}
-    for coord in coords:
-        blocks[coord] = model.new_int_var_from_domain(domains[coord], f'block({coord})')
-
-    # Constraint #2 - Number of pieces (distinct values) in the solution must equal 9
-    piece_usage = []
-    for p_id in range(npieces):
-        p_used = model.new_bool_var(f'used({p_id})')
-        model.add(p_used == any(var == p_id for var in blocks.values()))
-        piece_usage.append(p_used)
-    model.add(sum(piece_usage) == 9)
-
-    solver = cp_model.CpSolver()
-    if DEBUG:
-        solver.parameters.log_search_progress = True
-        if DEBUG > 1:
-            solver.parameters.log_subsolver_statistics = True
-    status = solver.solve(model)
-    print(f"Status: {status} ({solver.status_name(status)})", file=sys.stderr)
-
-    if status not in (cp_model.OPTIMAL, cp_model.FEASIBLE):
+    model = Model0(pieces)
+    model.build()
+    succ = model.solve()
+    if not succ:
         return None
-
-    print("\nSolver Stats", file=sys.stderr)
-    print(f"- Conflicts : {solver.num_conflicts}", file=sys.stderr)
-    print(f"- Branches  : {solver.num_branches}", file=sys.stderr)
-    print(f"- Wall time : {solver.wall_time:.2f} secs", file=sys.stderr)
-
-    solution = [str(p_used) for p_used in piece_usage if solver.value(p_used)]
-    return solution
+    model.print_stats()
+    return model.solution()
 
 ########
 # main #
